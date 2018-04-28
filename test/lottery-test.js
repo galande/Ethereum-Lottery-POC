64b0dc2337ebc11e37f2baad3288f13e5c1183d9
galande <bgalande250@gmail.com>
2018-04-28 22:09:23 +0530

    Added tests in testfile
    Test scenarios added

--- test/lottery-test.js
+++ test/lottery-test.js
@@ -25,7 +25,7 @@ describe('Lottery Contract', ()=>{
         assert.ok(lottery.options.address)
     });
 
-    it('Should add 1 player', async () =>{
+    it('Should allow to enter 1 player', async () =>{
 
         await lottery.methods.enter().send({
             from: accounts[0], 
@@ -36,5 +36,40 @@ describe('Lottery Contract', ()=>{
         assert.equal(accounts[0], players[0]);
         assert.equal(1, players.length);
 
+    });
+
+
+    it('Should allow to enter multiple player', async () =>{
+
+        await lottery.methods.enter().send({
+            from: accounts[0], 
+            value: web3.utils.toWei('0.02', 'ether')});
+        
+        await lottery.methods.enter().send({
+            from: accounts[1], 
+            value: web3.utils.toWei('0.02', 'ether')});
+
+        await lottery.methods.enter().send({
+            from: accounts[2], 
+            value: web3.utils.toWei('0.02', 'ether')});
+
+        const players = await lottery.methods.getPlayers().call({from: accounts[0]});
+
+        assert.equal(accounts[0], players[0]);
+        assert.equal(accounts[1], players[1]);
+        assert.equal(accounts[2], players[2]);
+        assert.equal(3, players.length);
+
+    });
+
+    it('Requires minimum amount of ether to enter', async ()=>{
+        try {
+            await lottery.methods.enter().send({
+                from: accounts[0], 
+                value: 0});
+            assert(false);
+        } catch (err) {
+            assert(err);
+        }
     });
 });
